f83af55745b499c76cfaca3ee21c644dd143c986
Antonio Gonçalves Dias <antonio_g_d@hotmail.com>
2019-01-18 22:57:58 -0200

    Using typescript

---
+++ src/app/hello-typescript/main.js
@@ -0,0 +1,22 @@
+var myVar = 'My Variable';
+function myFunction(x, y) {
+    return x + y;
+}
+// ES 6 or ES 2015
+var num = 2;
+var PI = 3.14;
+var numbers = [1, 2, 3];
+numbers.map(function (value) {
+    return value * 2;
+});
+numbers.map(function (value) { return value * 2; });
+var MathClass = /** @class */ (function () {
+    function MathClass() {
+    }
+    MathClass.prototype.sum = function (x, y) {
+        return x + y;
+    };
+    return MathClass;
+}());
+var n1 = 'sdfsdf';
+n1 = 4;
---
+++ src/app/hello-typescript/main.ts
@@ -0,0 +1,25 @@
+var myVar = 'My Variable';
+
+function myFunction(x, y) {
+    return x + y;
+}
+
+// ES 6 or ES 2015
+let num = 2;
+const PI = 3.14;
+
+var numbers = [1, 2, 3];
+numbers.map(function(value) {
+    return value * 2;
+});
+
+numbers.map(value => value * 2);
+
+class MathClass {
+    sum(x, y) {
+        return x + y;
+    }
+}
+
+var n1: any = 'sdfsdf';
+n1 = 4;
