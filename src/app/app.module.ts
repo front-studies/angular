f4a5423387a6d0996938df2797fab22da511e5a1
Antonio Gonçalves Dias <antonio_g_d@hotmail.com>
2019-01-19 23:35:10 -0200

    Modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MyFirstComponent } from './my-first/my-first.component';
 import { MyFirst2Component } from './my-first2/my-first2.component';
+import { CoursesModule } from './courses/courses.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     MyFirstComponent,
-    MyFirst2Component
+    MyFirst2Component,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CoursesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
